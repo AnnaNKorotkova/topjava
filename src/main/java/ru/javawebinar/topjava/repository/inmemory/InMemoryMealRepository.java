7bdbca743a09fa16aa575acb47e6b5a0bb920a8d
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2020-03-31 09:47:10 +0300

    HW2

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,55 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    @Autowired
+    private Map<Integer, List<Meal>> repository = new ConcurrentHashMap<>();
+    @Autowired
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        for (Meal MEAL : MealsUtil.MEALS) save(MealsUtil.USER.getId(), MEAL);
     }
 
+
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            repository.get(userId).add(meal);
             return meal;
         }
-        // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        repository.get(userId).add(meal);
+        return meal;
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int userId, int mealId) {
+        return repository.get(userId).remove(mealId) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int mealId) {
+        return repository.get(userId).get(mealId);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        return repository.get(userId).stream()
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,54 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Controller
 public class MealRestController {
+
+    @Autowired
     private MealService service;
 
+    public Meal create(int userId, Meal meal) {
+        return service.create(userId, meal);
+    }
+
+    public void delete(int userId, int id) {
+        checkNotFoundWithId(service.delete(userId, id), id);
+    }
+
+    public Meal get(int userId, int id) {
+        return checkNotFoundWithId(service.get(userId, id), id);
+    }
+
+    public List<MealTo> getAll(User user) {
+        List<Meal> list = new ArrayList<>(service.getAll(user.getId()));
+        if (list.isEmpty()){
+            return new ArrayList<>();
+        } else {
+            return MealsUtil.getTos(list, user.getCaloriesPerDay());
+        }
+    }
+
+    public List<MealTo> getAllByTime (LocalTime startTime, LocalTime endTime, User user){
+        List<Meal> list = new ArrayList<>(service.getAll(user.getId()));
+        if (list.isEmpty()){
+            return new ArrayList<>();
+        } else {
+            return MealsUtil.getFilteredTos(list, user.getCaloriesPerDay(),startTime,endTime);
+        }
+    }
 }
