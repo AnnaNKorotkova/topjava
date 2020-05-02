2678655168769750ec3319198b99d5b74b651146
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2020-05-02 10:25:55 +0300

    HW6

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -0,0 +1,109 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.JpaUtil;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.repository.jdbc.JdbcUserRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import static ru.javawebinar.topjava.UserTestData.*;
+
+@ActiveProfiles("noCache")
+public abstract class AbstractUserServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    protected UserService service;
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    protected JpaUtil jpaUtil;
+    @Autowired
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    private UserRepository repository;
+    @Autowired
+    private CacheManager cacheManager;
+
+
+    @Before
+    public void setUp() throws Exception {
+        cacheManager.getCache("users").clear();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+
+    @Test
+    public void create() throws Exception {
+        User newUser = getNew();
+        User created = service.create(newUser);
+        Integer newId = created.getId();
+        newUser.setId(newId);
+        USER_MATCHER.assertMatch(created, newUser);
+        USER_MATCHER.assertMatch(service.get(newId), newUser);
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void duplicateMailCreate() throws Exception {
+        service.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.ROLE_USER));
+    }
+
+    public void delete() throws Exception {
+        service.delete(USER_ID);
+        Assert.assertNull(repository.get(USER_ID));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deletedNotFound() throws Exception {
+        service.delete(1);
+    }
+
+    @Test
+    public void get() throws Exception {
+        User user = service.get(USER_ID);
+        USER_MATCHER.assertMatch(user, USER);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getNotFound() throws Exception {
+        service.get(1);
+    }
+
+    @Test
+    public void getByEmail() throws Exception {
+        User user = service.getByEmail("user@yandex.ru");
+        USER_MATCHER.assertMatch(user, USER);
+    }
+
+    @Test
+    public void update() throws Exception {
+        User updated = getUpdated();
+        service.update(updated);
+        USER_MATCHER.assertMatch(service.get(USER_ID), updated);
+        System.out.println(10);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<User> all = service.getAll();
+        USER_MATCHER.assertMatch(all, ADMIN, USER);
+    }
+
+    @Test
+    public void createWithException() throws Exception {
+        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Set.of())), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Set.of())), ConstraintViolationException.class);
+    }
+}
