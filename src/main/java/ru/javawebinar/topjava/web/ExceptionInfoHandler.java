c5023dc77d9d84e7b41ff8d169f0176e4cb384df
AnnaNKorotkova <korotkova.a.n@yandex.ru>
2020-06-07 10:21:45 +0300

    HW10 1.16 remove imports

--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -21,9 +21,7 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
